d00029f8c5ef8425ff1338d4c4ef9c2c30b09d2d
khanghuynh99 <122740460+khanghuynh99@users.noreply.github.com>
2023-12-04 19:48:17 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,16 +4,27 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+const originalNoBtnPosition = {
+    left: noBtn.style.left,
+    top: noBtn.style.top
+  };
+
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay!!! see you on the 16th!";
   gif.src =
     "https://media.giphy.com/media/13CoXDiaCcCoyk/giphy.gif";
+
+   noBtn.style.left = originalNoBtnPosition.left;
+   noBtn.style.top = originalNoBtnPosition.top;
+
 });
 
 noBtn.addEventListener("click", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
+
+  question.innerHTML = "missed the button nerd"
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
